b8476f97027efdc138f17cd3332461c925006e41
Bobby Burton <bobby.burton@protonmail.com>
2022-06-18 18:59:36 +0100

    .

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-cp /opt/minecraft/eula.txt /home/minecraft/eula.txt
-cp /opt/minecraft/server.properties /home/minecraft/server.properties
+if [ -f "/home/minecraft/eula.txt" ]; then
+  echo "EULA exists";
+else
+  cp /opt/minecraft/eula.txt /home/minecraft/eula.txt
+fi
 
-sed 's,{{SERVER_NAME}},'"${SERVER_NAME}"',g' -i /home/minecraft/server.properties
-sed 's,{{VIEW_DISTANCE}},'"${VIEW_DISTANCE}"',g' -i /home/minecraft/server.properties
-sed 's,{{MOTD}},'"${MOTD}"',g' -i /home/minecraft/server.properties
+if [ -f "/home/minecraft/server.properties" ]; then
+  echo "Properties exists";
+else
+  cp /opt/minecraft/server.properties /home/minecraft/server.properties
+  sed 's,{{SERVER_NAME}},'"${SERVER_NAME}"',g' -i /home/minecraft/server.properties
+  sed 's,{{VIEW_DISTANCE}},'"${VIEW_DISTANCE}"',g' -i /home/minecraft/server.properties
+  sed 's,{{MOTD}},'"${MOTD}"',g' -i /home/minecraft/server.properties
+if
 
 bash /opt/minecraft/start.sh
